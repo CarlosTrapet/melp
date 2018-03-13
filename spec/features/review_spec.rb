03251d4e8bdd889f617c8eef5f37982f1ea8910e
Noel Vock <noel1uk@gmail.com>
2018-03-13 15:50:23 +0000

    Pass Review feature test.

--- spec/features/review_spec.rb
+++ spec/features/review_spec.rb
@@ -1,10 +1,13 @@
 require 'rails_helper.rb'
 feature "Reviews" do
   scenario "user can add restaurant reviews" do
-    visit '/restaurants'
-    click_link 'Review'
-    fill_in 'input', with: 'Yummy yummy food'
-    click_on 'Submit'
+    visit '/restaurants/new'
+    fill_in :"restaurant[name]", with: "The Fat Duck"
+    fill_in :"restaurant[description]", with: "Avante garde snacks"
+    click_button "Save Restaurant"
+    fill_in 'review[reviewer]', with: 'Luke'
+    fill_in 'review[body]', with: 'Yummy yummy food'
+    click_on 'Create Review'
     expect(page).to have_content('Yummy yummy food')
   end
 end
