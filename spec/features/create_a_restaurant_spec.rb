4b42024029e77190b7ef18e40f4fb2b6b18bef78
Stephen Tilley <stephentilley@gmail.com>
2018-03-13 15:20:27 +0000

    create a restaurant test now passing

--- spec/features/create_a_restaurant_spec.rb
+++ spec/features/create_a_restaurant_spec.rb
@@ -6,7 +6,7 @@ feature 'Create a restaurant' do
     fill_in :"restaurant[name]", with: "The Fat Duck"
     fill_in :"restaurant[description]", with: "Avante garde snacks"
     click_button "Save Restaurant"
-    expect(current_path).toEqual "restaurants/1"
+    expect(page).to have_current_path "/restaurants/1"
     expect(page).to have_content "The Fat Duck"
     expect(page).to have_content "Avante garde snacks"
   end
