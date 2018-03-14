b2cf6053e3e54960399f9ceafe6f3bc7625bd51b
Noel Vock <noel1uk@gmail.com>
2018-03-14 14:23:58 +0000

    Created edit restaurant spec. Attempt to seed test database table.

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -4,18 +4,30 @@ class RestaurantsController < ApplicationController
     @restaurants = Restaurant.all
   end
 
-  def show
-    @restaurant = Restaurant.find(params[:id])
-  end
-
-  def new
-  end
-
   def create
     @restaurant = Restaurant.new(restaurant_params)
     @restaurant.save
     redirect_to @restaurant
   end
+
+  def new
+  end
+
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    p Restaurant.methods
+  end
+
+
+
 
   private
     def restaurant_params
